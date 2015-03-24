da7b953433c725e70e88b0370d3bfec4d1c6bf4c
Jonathan Yeong <jonathanyeong@gmail.com>
2015-03-24 20:53:23 +1100

    Add trello configuration to gem
    This commit should print out all the boards you have in your account.

--- lib/trello_newsletter.rb
+++ lib/trello_newsletter.rb
@@ -1,5 +1,12 @@
 require "trello_newsletter/version"
+require "trello"
 
-module TrelloNewsletter
-  # Your code goes here...
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+  config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+end
+
+class TrelloNewsletter
+  boards = Trello::Board.all
+  puts boards
 end
