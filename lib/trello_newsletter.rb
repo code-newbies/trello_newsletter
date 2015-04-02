cb858db17631549015b44c29a005c174186efeb2
Jonathan Yeong <jonathanyeong@gmail.com>
2015-04-03 08:23:57 +1100

    Initialise Post class
    Other changes:
    
    - Make trello_newsletter version refer to class TrelloNewsletter. I'm
      not sure if this is a gem thing...
    - Remove commented out meta class. We know this works in a previous
      commit.
    
    TODO:
    - Spit out a html file.

--- lib/trello_newsletter.rb
+++ lib/trello_newsletter.rb
@@ -1,5 +1,6 @@
-#require "trello_newsletter/version"
+require_relative "trello_newsletter/version"
 require_relative "meta.rb"
+require_relative "post.rb"
 require "trello"
 require "pry"
 
@@ -7,43 +8,6 @@ Trello.configure do |config|
   config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
   config.member_token = ENV['TRELLO_MEMBER_TOKEN']
 end
-
-#class Meta
-#  attr_reader :cards
-#  def initialize(list)
-#    @cards = list.cards
-#  end 
-#
-#  def header_image
-#    text = find_card("Header Image").desc
-#    # removing the tags because we just want the link
-#    md_to_html(text).gsub("\n<p>","").gsub("</p>\n","")
-#  end
-#
-#  def published_at
-#    find_card("Date").desc
-#  end
-#
-#  def intro_text
-#    text = find_card("Intro").desc
-#    md_to_html(text)
-#  end
-#
-#  def outro_text
-#    text = find_card("Outro").desc
-#    md_to_html(text)
-#  end
-#  
-#  private
-#
-#  def find_card(name)
-#    cards.find { |card| card.name == name }
-#  end
-#
-#  def md_to_html(text)
-#    Maruku.new(text).to_html
-#  end
-#end
 
 class TrelloNewsletter
   boards = Trello::Board.all
@@ -53,5 +17,10 @@ class TrelloNewsletter
   lists = board.lists
   meta_list = lists.select { |n| n.attributes[:name] == "Meta" }.first
   meta = Meta.new(meta_list)
+  headlines_list = lists.select { |n| n.attributes[:name] == "Headlines" }.first
+  headlines_list.cards.each do |card|
+    post = Post.new(card)
+    binding.pry
+  end
 end
 
--- lib/trello_newsletter/version.rb
+++ lib/trello_newsletter/version.rb
@@ -1,3 +1,3 @@
-module TrelloNewsletter
+class TrelloNewsletter
   VERSION = "0.1.0"
 end
