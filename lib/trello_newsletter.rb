b63cd3a146a347229ab6b44f44083f828f08be82
Jonathan Yeong <jonathanyeong@gmail.com>
2015-04-04 06:52:53 +1100

    Add a "generate" newsletter rake task
    This commit puts the code in Trello Newsletter class into functions.
    Also bump the version of ruby-trello gem in the gemfile so that the most
    recent version could be used.

--- lib/trello_newsletter.rb
+++ lib/trello_newsletter.rb
@@ -10,38 +10,44 @@ Trello.configure do |config|
 end
 
 class TrelloNewsletter
-  boards = Trello::Board.all
-  puts boards.count
-  board_name = "CodeNewbie Newsletter"
-  board = Trello::Board.all.find { |b| b.name == board_name }
-  lists = board.lists
+  def generate
+    boards = Trello::Board.all
+    puts boards.count
+    board_name = "CodeNewbie Newsletter"
+    board = Trello::Board.all.find { |b| b.name == board_name }
+    lists = board.lists
 
-  meta_list = lists.select { |n| n.attributes[:name] == "Meta" }.first
-  meta = Meta.new(meta_list)
+    meta_list = lists.select { |n| n.attributes[:name] == "Meta" }.first
+    meta = Meta.new(meta_list)
 
-  headlines_list = lists.select { |n| n.attributes[:name] == "Headlines" }.first
-  template = File.open("index.html", "w")
-  template.puts <<-DOC.gsub(/^ {4}/, '')
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <img src=#{meta.header_image} alt="Header Image">
-      <p>Published at: #{meta.published_at}</p>
-      <p>#{meta.intro_text}</p>
-  DOC
-  headlines_list.cards.each do |card|
-    post = Post.new(card)
-    template.puts "   <div>"
-    template.puts "    <h2>#{post.title}</h2>"
-    template.puts "    #{post.body}"
-    template.puts "   </div>"
+    headlines_list = lists.select { |n| n.attributes[:name] == "Headlines" }.first
+    html_output(meta, headlines_list)
+    puts "Finished generating issue"
   end
-  template.puts "<p>#{meta.outro_text}</p>"
-  template.puts "FINISHED!"
-  template.close
-  puts "Finished generating issue"
+
+  def html_output(meta, headlines_list)
+    template = File.open("index.html", "w")
+    template.puts <<-DOC.gsub(/^ {4}/, '')
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <img src=#{meta.header_image} alt="Header Image">
+        <p>Published at: #{meta.published_at}</p>
+        <p>#{meta.intro_text}</p>
+    DOC
+    headlines_list.cards.each do |card|
+      post = Post.new(card)
+      template.puts "   <div>"
+      template.puts "    <h2>#{post.title}</h2>"
+      template.puts "    #{post.body}"
+      template.puts "   </div>"
+    end
+    template.puts "<p>#{meta.outro_text}</p>"
+    template.puts "FINISHED!"
+    template.close
+  end
 end
 
