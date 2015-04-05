ac54da00fd620a39d15d1c8d362dc8fe592e36f9
Jonathan Yeong <jonathanyeong@gmail.com>
2015-04-05 10:40:04 +1000

    Replace minitest tests with rspec tests
    Add basic test to check a newsletter can be generated. VCR gem is used
    to speed up the tests.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require "rspec"
+require "rspec/core"
+require "vcr"
+require "pry"
+require_relative "../lib/trello_newsletter.rb"
+# Dir["#{Dir.pwd}/lib/*.rb"].each {|file| require file}
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+  end
+
+  config.order = :random
+end
