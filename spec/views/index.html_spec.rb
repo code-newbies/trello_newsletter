be0c849b9bd0120cd8538f85f7aa8f6c6473cbd1
Jonathan Yeong <jonathanyeong@gmail.com>
2015-06-03 07:08:58 +1000

    Implement initial tests for index.html
    This test will check if the head and body tags are in the html.

---
+++ spec/views/index.html_spec.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+
+describe "html output" do 
+  before :all do
+    html_file = File.open("index.html")
+    @page = Nokogiri::HTML(html_file)
+    html_file.close
+  end
+
+  it "contains a head tag" do
+    # at_css will only return a single result
+    expect(@page.css('head').count).to eq(1)
+  end
+
+  it "contains a body tag" do
+    expect(@page.css('body').count).to eq(1)
+  end
+end
